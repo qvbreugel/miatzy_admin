edefe2fd18653291af6311a2b3eb340062827392
qvbreugel <qvbreugel@gmail.com>
2018-12-18 22:33:24 +0100

    Finished Receive Page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  database: "miatzy"
+});
+
+function getConnection() {
+  return pool;
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/receive", function(req, res, next) {
+  const connection = getConnection();
+
+  const ticketNumber = req.body.ticketNumber;
+  const product_id = req.body.product_id;
+
+  const queryString =
+    "UPDATE products SET available = 'yes' WHERE ticketnumber = ? AND product_id = ?";
+
+  connection.query(queryString, [ticketNumber, product_id], function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+
+    res.send({ Received: true });
+  });
 });
 
 module.exports = router;
