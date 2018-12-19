9da112efbae4e0a1818f71f0c3b8d659e03d012d
qvbreugel <qvbreugel@gmail.com>
2018-12-19 02:59:37 +0100

    Minimum Viable Product

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.post("/sell", function(req, res, next) {
   const product_id = req.body.product_id;
 
   const queryString =
-    "UPDATE products SET sold = 'true' WHERE ticketnumber = ? AND product_id = ?";
+    "UPDATE products SET sold = 'true' WHERE ticketnumber = ? AND product_id = ? AND available = 'yes'";
 
   connection.query(queryString, [ticketNumber, product_id], function(
     error,
@@ -51,6 +51,41 @@ router.post("/sell", function(req, res, next) {
     if (error) throw error;
 
     res.send({ Sold: true });
+  });
+});
+
+router.post("/pay", function(req, res, next) {
+  const connection = getConnection();
+  let unSold = [];
+  let Sold = [];
+
+  const ticketNumber = req.body.ticketNumber;
+
+  const queryString =
+    "SELECT * FROM products WHERE ticketnumber = ? AND SOLD = 'false' AND available = 'yes'";
+
+  connection.query(queryString, [ticketNumber], function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    unSold = results;
+
+    const soldQueryString =
+      "SELECT * FROM products WHERE ticketnumber = ? AND sold = 'true'";
+    connection.query(soldQueryString, [ticketNumber], function(
+      error,
+      results,
+      fields
+    ) {
+      if (error) throw error;
+      Sold = results;
+
+      const data = { unSold, Sold };
+      console.log(data);
+      res.send(data);
+    });
   });
 });
 
